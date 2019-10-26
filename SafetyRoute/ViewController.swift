ebe333a98e8759f74e371f6abb84cee4c27d7f4b
Namratha Prithviraj <namrathap@eduroam-169-233-217-44.ucsc.edu>
2019-10-26 12:03:02 -0700

    Initial commit

---
+++ SafetyRoute/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  SafetyRoute
+//
+//  Created by Namratha Prithviraj on 10/26/19.
+//  Copyright © 2019 Namratha Prithviraj. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+import GooglePlaces
+
+class ViewController: UIViewController {
+    
+    private let locationManager = CLLocationManager()
+    
+    // You don't need to modify the default init(nibName:bundle:) method.
+    
+    override func viewDidLoad() {
+        locationManager.delegate = self as! CLLocationManagerDelegate
+        locationManager.requestWhenInUseAuthorization()
+
+    }
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    // 2
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // 3
+        guard status == .authorizedWhenInUse else {
+            return
+        }
+        // 4
+        locationManager.startUpdatingLocation()
+        
+        //5
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+    }
+    
+    // 6
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
+        }
+        
+        // 7
+        mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+        
+        // 8
+        locationManager.stopUpdatingLocation()
+    }
+}
